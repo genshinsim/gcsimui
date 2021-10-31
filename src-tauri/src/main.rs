c785aee4e10f512ce75ed8e474cadf7e3a57d47f
genshinsim <85884307+genshinsim@users.noreply.github.com>
2021-10-31 00:28:41 -0400

    bug fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -29,17 +29,20 @@ fn main() {
   .add_submenu(Submenu::new(
     "File",
     Menu::new()
-      .add_item(custom_menu("Open...").accelerator("cmdOrControl+O"))
-      .add_native_item(MenuItem::Separator)
+      // .add_item(custom_menu("Open...").accelerator("cmdOrControl+O"))
+      // .add_native_item(MenuItem::Separator)
       .add_item(custom_menu("Close").accelerator("cmdOrControl+W"))
-      .add_item(custom_menu("Save").accelerator("cmdOrControl+S"))
-      .add_item(custom_menu("Save As...").accelerator("shift+cmdOrControl+S")),
+      // .add_item(custom_menu("Save").accelerator("cmdOrControl+S"))
+      // .add_item(custom_menu("Save As...").accelerator("shift+cmdOrControl+S")),
   ))
   .add_submenu(Submenu::new("Edit", {
     let mut menu = Menu::new();
+    #[cfg(target_os = "macos")]
+    {
     menu = menu.add_native_item(MenuItem::Undo);
     menu = menu.add_native_item(MenuItem::Redo);
     menu = menu.add_native_item(MenuItem::Separator);
+    }
     menu = menu.add_native_item(MenuItem::Cut);
     menu = menu.add_native_item(MenuItem::Copy);
     menu = menu.add_native_item(MenuItem::Paste);
@@ -49,18 +52,18 @@ fn main() {
     }
     menu = menu.add_native_item(MenuItem::SelectAll);
     menu
-  }))
-  .add_submenu(Submenu::new("View", Menu::new()))
-  .add_submenu(Submenu::new(
-    "Window",
-    Menu::new()
-      .add_native_item(MenuItem::Minimize)
-      .add_native_item(MenuItem::Zoom),
-  ))
-  .add_submenu(Submenu::new(
-    "Help",
-    Menu::new().add_item(custom_menu("Learn More")),
-  ));
+  }));
+  // .add_submenu(Submenu::new("View", Menu::new()))
+  // .add_submenu(Submenu::new(
+  //   "Window",
+  //   Menu::new()
+  //     .add_native_item(MenuItem::Minimize)
+  //     .add_native_item(MenuItem::Zoom),
+  // ))
+  // .add_submenu(Submenu::new(
+  //   "Help",
+  //   Menu::new().add_item(custom_menu("Learn More")),
+  // ));
   // .add_native_item(MenuItem::Copy);
 
   tauri::Builder::default()

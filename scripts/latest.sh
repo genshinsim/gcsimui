8fce31a0a3bf5dfecf43f762af5f16ea86f15c27
genshinsim <85884307+genshinsim@users.noreply.github.com>
2021-11-01 16:28:50 -0400

    update workflow

---
+++ scripts/latest.sh
@@ -0,0 +1,40 @@
+
+arr=()
+IFS=$'\n' read -r -d '' -a arr < <( curl -s "https://api.github.com/repos/genshinsim/gsimui/releases/latest" | jq -r '.assets[] | .browser_download_url' && printf '\0' )
+
+version=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
+echo "Latest version $version"
+darwin=""
+win64=""
+linux=""
+
+for i in "${arr[@]}"
+do
+#    echo "$i"
+   # or do whatever with individual element of the array
+   [[ $i =~ _x64.msi.zip$ ]] && win64=$i && echo "Windows build found: $win64"
+   [[ $i =~ app.tar.gz$ ]] && darwin=$i && echo "Darwin build found: $darwin"
+   [[ $i =~ AppImage.tar.gz$ ]] && linux=$i && echo "Linux build found: $linux"
+done
+
+
+cat << EOF > latest.json
+{
+    "name":"v$version",
+    "notes":"See release notes at https://github.com/genshinsim/gsimui/releases",
+    "platforms": {
+        "darwin": {
+            "signature":"",
+            "url":"$darwin"
+        },
+        "linux": {
+            "signature":"",
+            "url":"$linux"
+        },
+        "win64": {
+            "signature":"",
+            "url":"$win64"
+        }
+    }
+}
+EOF
